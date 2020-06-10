99438cf17b762724008770c6d1da66d580729b25
Findaaron <aarondean86@outlook.com>
2020-06-10 20:27:43 +0800

    Fix a bug about Timer15 demo.

--- iOSDemo/iOSDemo/Timer15.swift
+++ iOSDemo/iOSDemo/Timer15.swift
@@ -5,6 +5,9 @@
 //  Created by 丁海能 on 2020/6/8.
 //  Copyright © 2020 Aaron Dean. All rights reserved.
 //
+/*
+实时预览只有秒在动,分未动.使用模拟器可以正常倒计时.
+*/
 
 import SwiftUI
 
@@ -25,73 +28,72 @@ struct Timer15: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack{
-                Text("一刻")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                ZStack(alignment: .center){
-                        Circle()
-                            .fill(Color.black.opacity(0.9))
-                            .frame(width:UIScreen.main.bounds.width / 1.4, height: UIScreen.main.bounds.height / 2.2)
-        
-                        VStack{
-                            Text("剩余 \(minutesRemain) 分 : \(secondsRemain) 秒").font(.title).foregroundColor(.white)
-                        }
-                    }
-                HStack(spacing: UIScreen.main.bounds.width / 5 - 20){
-                        //Mark: - 开始
-                        Button(action: {
-                            self.minutesRemain = 14
-                            self.secondsRemain = 59
-                        }){
+                    Text("一刻")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                    ZStack(alignment: .center){
+                            Circle()
+                                .fill(Color.white.opacity(0.4))
+                                .frame(width:UIScreen.main.bounds.width / 1.5, height: UIScreen.main.bounds.height / 2.5)
+            
                             VStack{
-                                Image(systemName: "play.circle").resizable()
-                                .frame(width: 40, height: 40)
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.white)
-                                Text("开始").font(.headline).foregroundColor(.white)
+                                Text("剩余 \(minutesRemain) 分 : \(secondsRemain) 秒").font(.title).foregroundColor(.primary)
                             }
                         }
-                        //Mark: - 重置
-                        Button(action: {
-                            self.minutesRemain = 0
-                            self.secondsRemain = 0
-                        }){
-                            VStack{
-                                Image(systemName: "multiply.circle").resizable()
-                                .frame(width: 40, height: 40)
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.white)
-                                Text("置零").font(.headline).foregroundColor(.white)
+                    HStack(spacing: UIScreen.main.bounds.width / 5 - 20){
+                            //Mark: - 开始
+                            Button(action: {
+                                self.minutesRemain = 14
+                                self.secondsRemain = 59
+                            }){
+                                VStack{
+                                    Image(systemName: "play.circle").resizable()
+                                    .frame(width: 40, height: 40)
+                                        .aspectRatio(contentMode: .fit)
+                                        .foregroundColor(.white)
+                                    Text("开始").font(.headline).foregroundColor(.white)
+                                }
+                            }
+                            //Mark: - 重置
+                            Button(action: {
+                                self.minutesRemain = 0
+                                self.secondsRemain = 0
+                            }){
+                                VStack{
+                                    Image(systemName: "multiply.circle").resizable()
+                                    .frame(width: 40, height: 40)
+                                        .aspectRatio(contentMode: .fit)
+                                        .foregroundColor(.white)
+                                    Text("置零").font(.headline).foregroundColor(.white)
+                                }
                             }
                         }
                     }
-                }
-                .onReceive(minutesTimer) { _ in
-                    guard self.isActive else { return }
-                    if self.minutesRemain > 0 && self.secondsRemain == 0{
-                         self.minutesRemain -= 1
+                    .onReceive(minutesTimer) { _ in
+                        guard self.isActive else { return }
+                        if self.minutesRemain > 0 && self.secondsRemain == 0{
+                             self.minutesRemain -= 1
+                        }
                     }
-                }
-                .onReceive(secondsTimer) { _ in
-                    guard self.isActive else { return }
-                    if self.minutesRemain > 0 && self.secondsRemain == 0{
-                         self.secondsRemain = 59
-                    }
+                    .onReceive(secondsTimer) { _ in
+                        guard self.isActive else { return }
+                        if self.minutesRemain > 0 && self.secondsRemain == 0{
+                             self.secondsRemain = 59
+                        }
 
-                    if self.secondsRemain > 0{
-                        self.secondsRemain -= 1
+                        if self.secondsRemain > 0{
+                            self.secondsRemain -= 1
+                        }
                     }
-                }
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-                    self.isActive = false
-                }
-        }
+                    .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                        self.isActive = false
+                    }        }
         }
 }
 
-struct MeditationTimer_Previews: PreviewProvider {
+struct Timer15_Previews: PreviewProvider {
     static var previews: some View {
         Timer15()
     }
